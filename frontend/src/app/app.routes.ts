fb6447296fed786b1fc90e70cc357442af33d161
baha <baha.edabdelkhalek@gmail.com>
2026-06-07 13:02:32 +0100

    config(frontend): environnements dev/prod, configuration Apollo GraphQL et routage lazy-load

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+	{
+		path: '',
+		loadComponent: () =>
+			import('./features/todos/todo-list/todo-list.component').then(
+				(m) => m.TodoListComponent
+			),
+	},
+	{ path: '**', redirectTo: '' },
+];
